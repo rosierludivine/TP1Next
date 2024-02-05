b32169620367474932dae0f454a0db2bb7b6f21b
rosierludivine <91051230+rosierludivine@users.noreply.github.com>
2024-02-05 15:23:50 +0100

    Chapitre 4:Terminer

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -38,7 +38,14 @@ export default function Page() {
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
-         />
+        />
+          <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+        />
         </div>
       </div>
     </main>
